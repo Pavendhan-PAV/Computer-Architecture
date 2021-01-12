5a71f4a8116167e95b59091ff8d030db7fad3c4a
Pavendhan <53622544+Pavendhan-PAV@users.noreply.github.com>
2021-01-12 11:47:25 +0530

    Add files via upload

--- Single Precision Floating point Adder/FPA.v
+++ Single Precision Floating point Adder/FPA.v
@@ -22,21 +22,36 @@ module top;
     Split SP2(Small,S2,E2,M2);
 
     //----------------------------------------------------------------------------------DIFFERENCE
-    wire  [7:0]Ediff; //EXP_DIFFERENCE
+    wire  [7:0]Ediff;
+    reg [7:0]Final_Ediff; //EXP_DIFFERENCE
     assign Ediff = E1 - E2;
+
+    
+    always@(*)
+    begin
+        if(Ediff>32'd24)
+        begin
+		    Final_Ediff=32'd24;
+        end
+        else
+        begin
+            Final_Ediff=Ediff;
+        end
+    end
 
     //----------------------------------------------------------------------------------DENORMALISE
     wire [31:0] N1,N2,N3;   //TEMPORARY VAR
     assign N1 = {|E1,M1};   //Reduction for handle zeroes
     assign N2 = {|E2,M2};   //and denormal numbers
-
-    Shift S(N2,Ediff,N3); //Smaller number is shifted 
+    assign N3[31:24] = {8'b0};
+    
+    //Shift S(N2,Ediff,N3);                                         //NORMAL RIGHT LOGICAL SHIFT    
+    barrel_logicalRight Br(N2[23:0],Final_Ediff[4:0],N3[23:0]);     //BARREL RIGHT LOGICAL SHIFT
 
     wire [31:0] N4; 
     reg [31:0]N5; //FINAL SMALLER number
 
     assign N4 = {32{S1^S2}}^N3; //1s COMPLEMENT if signs are different
-    //$display("N4: %b",N4);
     
     //----------------------------------------------------------------------------------SUM/SUBTRACTION
     wire [31:0]Sum;
@@ -51,16 +66,27 @@ module top;
 
     //-----------------------------------------------------------------------------------NORMALISE
 	reg [22:0] M3,Temp_Mantissa;
+    wire [22:0] Temp_M3;
     reg [7:0] E3;
 
     integer i =0;
-    always @(Sum)
+    barrel_logicalLeft Bl(Temp_Mantissa,i[4:0],Temp_M3);
+   
+    /*
+    always@(*)
+    begin
+            $display("M3: \n%b  %d\n",Temp_M3,i);
+    end
+    */
+
+    always @(Sum or Temp_M3)
     begin
         if(Sum[24]==1)      //OVERFLOW
         begin
-            M3 = Sum[23:1];
+            M3[22:0] = Sum[23:1];
             E3 = E1 + 1'b1;
         end
+
         else if(Sum[23]==0) //UNDERFLOW
         begin
             i = 1;
@@ -70,8 +96,10 @@ module top;
             end 
             E3 = E1 - i;
             Temp_Mantissa = Sum[22:0];
-            M3 = Temp_Mantissa<<i;
+            //M3 = Temp_Mantissa<<i;    //DIRECT LEFT LOGICAL SHIFT
+            M3=Temp_M3;                 //BARREL LEFT LOGICAL SHIFT
         end
+
         else    //NORMAL CASE
         begin   
             M3 = Sum[22:0];
@@ -85,27 +113,17 @@ module top;
         if(&E1 == 1'b1 && |M1 == 1'b0)  //INFINITY case
             Final_exp = {S1,{8{1'b1}},23'b0};
    
-        else //normal & NAN cases
+        else //NORMAL & NAN cases
             Final_exp = {S1,{8{|Sum}} & E3,M3};
     end
 
 	//---------------------------------------------------------------------------------------------TESTBENCH
     initial
     begin
-      /*  #0	Exp1={1'b0,{8{1'b1}},23'b0}; Exp2={1'b0,{7{1'b1}},24'b111011};
+        #0	Exp1={1'b0,{8{1'b1}},23'b0}; Exp2={1'b0,{7{1'b1}},24'b111011};
         #10 Exp1={9'b010000000,{23{1'b0}}}; Exp2={9'b010000001,{23{1'b0}}};
-        #20 Exp1=32'b00111111110011001100110011001101; Exp2=32'b10111111100110011001100110011010; */
-        // 6 + 8 = 14
-        #0  Exp1={1'b0,{8'b10000001},23'b10000000000000000000000}; Exp2={1'b0,{8'b10000010},23'b00000000000000000000000};
-
-        // 9.75 + 0.5625 = 10.3125
-        #10 Exp1={1'b0,{8'b10000010},23'b00111000000000000000000}; Exp2={1'b0,{8'b01111110},23'b00100000000000000000000};
-
-        // -6 + 8 = 2
-        #30 Exp1={1'b1,{8'b10000001},23'b10000000000000000000000}; Exp2={1'b0,{8'b10000010},23'b00000000000000000000000};
-
-          #40 Exp1={1'b1,{8'b10000001},23'b10000000000000000000000}; Exp2={1'b0,{8'b10000010},23'b00000000000000000000000};
-           #20 Exp1=32'b00111111110011001100110011001101; Exp2=32'b10111111100110011001100110011010;
+        #20 Exp1=32'b00111111110011001100110011001101; Exp2=32'b10111111100110011001100110011010;
+        
     end
     initial
     begin
